4ba9f47e3eb08452d846ec3c230884fde8e817a4
gregw94542 <gregw94542@gmail.com>
2018-02-24 16:25:10 -0800

    Add first pass of of conversion script

---
+++ scripts/cleanold.sh
@@ -0,0 +1,33 @@
+#!  /bin/bash
+
+echo "==================================================================="
+echo "WP TEMPLATE CONVERTER"
+echo "This script will take the the template plugin and convert the file names and contents to point"
+echo "  to a new plugin name"
+echo " this code expects that the template file "../../TZ_Insult" is poplulated with a working template"
+echo "==================================================================="
+
+SRC=$(cd ..; pwd)
+cp -r  ../../TZ_Insult/* ..
+
+echo -n "Enter name of plugin: "
+read PLUGIN
+echo Scanning $SRC - converting to $PLUGIN
+NAMETOFIX=$(find $SRC -name "TZ*"   -print)
+
+for name in $NAMETOFIX
+do
+	DEST=`echo $name | sed -e "s/TZ_Insult/$PLUGIN/"`
+	mv  $name  $DEST
+	cat $DEST | sed -e "s/TZ_Insult/$PLUGIN/" > xxx
+	mv xxx $DEST
+done
+
+NAMETOFIX=$(find $SRC -name "tz*"   -print)
+for name in $NAMETOFIX
+do
+	DEST=`echo $name | sed -e "s/tz_insult/$PLUGIN/"`
+	mv  $name  $DEST
+	cat $DEST | sed -e "s/tz_insult/$PLUGIN/" > xxx
+	mv xxx $DEST
+done
